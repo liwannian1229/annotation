06ac4b0a3d1f2a61d722109ccafef618837949b0
liwannian” <18790173051@163.com>
2020-10-10 21:28:05 +0800

    写着玩的

---
+++ common-utils/src/main/java/com/lwn/enums/ResourceCharacter.java
@@ -0,0 +1,19 @@
+package com.lwn.enums;
+
+/**
+ * @author liwannian
+ * @date 2020/10/10 21:18
+ */
+public class ResourceCharacter {
+
+    public static final String ONE = "1";
+
+    public static final String TWO = "2";
+
+    public static final String THREE = "3";
+
+    public static final String FOUR = "4";
+
+    public static final String FIVE = "5";
+    
+}
