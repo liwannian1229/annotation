230bde8a3e73816351d1e77c755825c8ddd5ad92
liwannian <18790173051@163.com>
2020-10-10 17:17:34 +0800

    测试一下

--- my-service/src/test/java/com/lwn/simple/TestOne.java
+++ my-service/src/test/java/com/lwn/simple/TestOne.java
@@ -11,7 +11,10 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.junit.Test;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 public class TestOne {
@@ -19,6 +22,89 @@ public class TestOne {
     @Test
     public void testBase() {
 
+    }
+
+    @Test
+    public void testA() {
+        int a = 5; // 0000 0101
+        int b = 3; // 0000 0011
+        a &= b; // 0000 0001
+        //  System.out.println(Integer.toBinaryString(a));
+
+        // 十进制转二进制输出
+        output(5);
+    }
+
+    public void output(int ten) {
+        StringBuilder outPrint = new StringBuilder();
+        int count = 0;
+        int begin = ten;
+        do {
+            count++;// 循环次数
+            int out = ten % 2;
+            if (out == 0) {
+                ten = ten / 2;
+                outPrint.append(0);
+            } else {
+                int temp = (ten - out) / 2;
+                if (temp != 0) {
+                    ten = temp;
+                    outPrint.append(out);
+                } else {
+                    outPrint.append(out);
+                    break;
+                }
+            }
+        } while (true);
+        outPrint.reverse();
+        System.out.println("循环次数: " + count);
+        System.out.println("十进制数: " + begin + " 转二进制: " + outPrint);
+    }
+
+    // 16位订单号
+    @Test
+    public void getOrderNo() {
+
+        final String letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        final String number = "0123456789";
+        Random random = new Random();
+        StringBuilder orderNo = new StringBuilder((letter.charAt(random.nextInt(letter.length())) + "") + (letter.charAt(random.nextInt(letter.length())) + ""));
+        for (int i = 0; i < 16; i++) {
+            String c = number.charAt(random.nextInt(number.length())) + "";
+            orderNo.append(c);
+        }
+        System.out.println("订单号:" + orderNo);
+    }
+
+    @Test
+    public void testHttpReq() {
+        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        assert sra != null;
+        HttpServletRequest req = sra.getRequest();
+        org.apache.commons.lang.StringUtils.isBlank(""); // obj.trim().length()==0
+        org.apache.commons.lang.StringUtils.isEmpty("");
+
+    }
+
+    // 随机生成11位手机号
+    @Test
+    public void testGeneratePhone() {
+        final String first = "1";
+        // final String[] seconds = {"3", "4", "5", "6", "7", "8", "9"};
+        final String second = "3456789";
+        // final String[] bodies = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        final String body = "0123456789";
+        StringBuilder phone = new StringBuilder();
+        Random random = new Random();
+        phone.append(first);
+        // phone.append(seconds[random.nextInt(seconds.length)]);
+        phone.append(second.charAt(random.nextInt(second.length())));
+        for (int i = 0; i < 9; i++) {
+            //    phone.append(bodies[random.nextInt(body.length())]);
+            phone.append(body.charAt(random.nextInt(body.length())));
+        }
+        System.out.println("长度为:" + phone.length());
+        System.out.println(phone);
     }
 
     @Test
